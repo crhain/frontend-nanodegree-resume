bfe9f3bbd0119f7356a6ee7676434b2379654d7a
Carl Hain <crhain@gmail.com>
2016-02-04 19:30:46 -0600

    Update js/resumeBuilder.js for lesson1

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,6 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
+ var awesomeThoughts = "I am Carl and I am AWESOME!";
+ var funThoughts = awesomeThoughts.replace("AWESOME", "FUN");
+ $("#main").append(funThoughts);
